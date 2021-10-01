767b10a0b9d2da2d1f66476de349e8eb11ad5f1c
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-10-02 00:56:09 +0300

    01.10.2021

--- CRUDAppAPI/Controllers/DCandidateController.cs
+++ CRUDAppAPI/Controllers/DCandidateController.cs
@@ -43,6 +43,7 @@ namespace WebAPI.Controllers
 
         // PUT: api/DCandidate/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutDCandidate(int id, DCandidate dCandidate)
         {
